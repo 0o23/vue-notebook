8cce7e17e45e3f73ab83b9ef003f0b747035d303
许俊杰 <3622326779@qq.com>
2023-05-22 18:37:02 +0800

    导入 element-ui、tailwindCSS

---
+++ src/layout/index.vue
@@ -0,0 +1,20 @@
+<template>
+	<el-container class="layout h-full">
+		<el-aside width="200px" class="bg-gray-200">
+			<Sidebar />
+		</el-aside>
+		<el-main class="p-2">Main</el-main>
+	</el-container>
+</template>
+
+<script>
+import Sidebar from './Sidebar.vue'
+
+export default {
+	components: {
+		Sidebar,
+	},
+}
+</script>
+
+<style></style>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,21 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 全局导入 ElementUI 组件库及其样式
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
+// 导入全局样式文件
+import '@/styles/index.css'
+
+// 导入 tailwindCSS
+import '@/styles/tailwind.css'
+
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+	router,
+	store,
+	render: h => h(App),
 }).$mount('#app')

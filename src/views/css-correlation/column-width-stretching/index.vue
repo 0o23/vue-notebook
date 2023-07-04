84a4d680b24e4eb0cbd9bf58863dc8491eef54cd
许俊杰 <3622326779@qq.com>
2023-07-04 14:01:12 +0800

    2

--- src/views/css-correlation/column-width-stretching/index.vue
+++ src/views/css-correlation/column-width-stretching/index.vue
@@ -1,15 +1,21 @@
+<!--
+@author: 许俊杰
+@desc: 可调节宽度的左右分栏
+@date: 2023/07/04
+-->
 <template>
-	<div class="column-width-stretching h-full">
-		<div class="resize-container" ref="resizeContainerRef">
-			<div class="resize-container-left" ref="resizeContainerLeftRef">
-				<div class="resize-bar"></div>
-				<div class="resize-line"></div>
-				<div class="resize-save">
-					<slot name="left">这是左边的内容</slot>
+	<div class="adjustable-left-right-columns">
+		<div class="container" id="adjustable-left-right-columns__container">
+			<div class="left">
+				<div class="resize"></div>
+				<div class="bar-btn"></div>
+				<div class="content">
+					<slot name="left"> 这是左边的内容 </slot>
 				</div>
 			</div>
-			<div class="resize-container-right" style="position: relative">
-				<slot name="right">这是右边的内容</slot>
+
+			<div class="right">
+				<slot name="right"> 这是左边的内容 </slot>
 			</div>
 		</div>
 	</div>
@@ -17,21 +23,78 @@
 
 <script>
 export default {
-	name: 'ColumnWidthStretching',
+	name: 'adjustableLeftRightColumns',
 
 	mounted() {
-		this.initResizeBarHeight()
+		this.initResizeHeightAndMaxWidth()
 	},
 
 	methods: {
-		initResizeBarHeight() {
-			const HEIGHT = this.$refs.resizeContainerRef.offsetHeight
-			this.$refs.resizeContainerLeftRef.style.height = HEIGHT + 'px'
+		initResizeHeightAndMaxWidth() {
+			const containerDOM = document.getElementById('adjustable-left-right-columns__container')
+			const resizeDOM = document.getElementsByClassName('resize')[0]
+
+			resizeDOM.style.height = containerDOM.offsetHeight + 'px'
+			resizeDOM.style.maxWidth = Math.floor(containerDOM.offsetWidth * 0.5) + 'px'
 		},
 	},
 }
 </script>
 
-<style>
-@import './styles/resize.scss';
+<style lang="scss" scoped>
+.adjustable-left-right-columns {
+	height: 100%;
+
+	.container {
+		display: flex;
+		height: 100%;
+	}
+
+	.left {
+		position: relative;
+	}
+
+	.left .resize {
+		min-width: 100px;
+		width: 300px;
+		resize: horizontal;
+		overflow: scroll;
+		height: inherit;
+		opacity: 0;
+	}
+
+	.resize::-webkit-scrollbar {
+		width: 8px;
+		height: inherit;
+	}
+	.resize:hover ~ .bar-btn,
+	.resize:active ~ .bar-btn {
+		border-left: 2px dashed skyblue;
+	}
+
+	.left .bar-btn {
+		position: absolute;
+		right: 0;
+		top: 0;
+		bottom: 0;
+		border-right: 2px solid #eee;
+		border-left: 1px solid #bbb;
+		pointer-events: none;
+	}
+
+	.content {
+		position: absolute;
+		top: 0;
+		z-index: 0;
+		overflow-x: hidden;
+		height: 100%;
+		width: calc(100% - 5px);
+	}
+
+	.right {
+		width: 200px;
+		flex: 1;
+		height: 200px;
+	}
+}
 </style>
